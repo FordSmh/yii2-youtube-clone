814996dc74e4111764aabd20f67a39d10ded63bc
fordsmh <vasyukvas@gmail.com>
2022-03-05 18:59:20 +0700

    First commit -- Yii framework deployment and Videos db and migration created

---
+++ backend/views/layouts/_header.php
@@ -0,0 +1,33 @@
+<?php
+use yii\bootstrap5\Html;
+use yii\bootstrap5\Nav;
+use yii\bootstrap5\NavBar;
+
+NavBar::begin([
+    'brandLabel' => Yii::$app->name,
+    'brandUrl' => Yii::$app->homeUrl,
+    'options' => [
+        'class' => 'navbar navbar-expand-md navbar-dark bg-dark',
+    ],
+]);
+$menuItems = [
+    ['label' => 'Upload video', 'url' => ['/site/index']],
+];
+if (Yii::$app->user->isGuest) {
+    $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+} else {
+    $menuItems[] = '<li>'
+        . Html::beginForm(['/site/logout'], 'post', ['class' => 'form-inline'])
+        . Html::submitButton(
+            'Logout (' . Yii::$app->user->identity->username . ')',
+            ['class' => 'btn btn-link logout']
+        )
+        . Html::endForm()
+        . '</li>';
+}
+echo Nav::widget([
+    'options' => ['class' => 'navbar-nav'],
+    'items' => $menuItems,
+]);
+NavBar::end();
+?>
---
+++ backend/views/layouts/_sidebar.php
@@ -0,0 +1,19 @@
+<?php echo \yii\bootstrap5\Nav::widget([
+        'options' => [
+                'class' => 'd-flex flex-column flex-shrink-0 p-3 bg-dark h-100 nav-tabs h-100'
+        ],
+        'items' => [
+            [
+                    'label' => 'Dashboard',
+                    'url' => ['site/index']
+            ],
+            [
+                    'label' => 'Videos',
+                    'url' => ['videos/index']
+            ],
+            [
+                    'label' => 'Comments',
+                    'url' => ['comments/index']
+            ]
+        ]
+])?>
